82239266c843322d397d2aa805f9d45b24e0b8a8
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-14 23:04:33 +0000

    Implemented tweet display in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,67 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// /*
+//  * Client-side JS logic goes here
+//  * jQuery is already loaded
+//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+//  */
+$(document).ready(function() {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1639347859407
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1639434259407
+    }
+  ];
+  // Test / driver code (temporary). Eventually will get this from the server.
+
+
+  const createTweetElement = function(data) {
+    return `<article class="tweet">
+  <header class="header">
+    <div><img src=${data.user.avatars}>${data.user.name}</div>
+    <div class="handle">${data.user.handle}</div>
+  </header>
+  <div class="tweet-text">${data.content.text}</div>
+  <footer class="tweet-footer small">
+    <line>${timeago.format(data.created_at)}</line>
+    <div>
+      <button class="tweet-butt"><i class="fas fa-flag"></i></button>
+      <button class="tweet-butt"><i class="fas fa-retweet"></i></button>
+      <button class="tweet-butt"><i class="fas fa-heart"></i></button>
+    </div>
+  </footer>
+</article>`;
+  };
+  
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $('#tweet-container').append($tweet); // t
+    }
+  };
+
+  renderTweets(tweetData);
+  
+});
+
+
+
+
