fcd46f69a208144ffd142fd5fe7e3639c1529f22
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-15 17:13:49 +0000

    minor cleanup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,34 +5,6 @@
 //  */
 $(document).ready(function() {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1639347859407
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1639434259407
-    }
-  ];
-  // Test / driver code (temporary). Eventually will get this from the server.
-
-
   const form = $('.tweet-form');
 
   form.submit(function(event) {
