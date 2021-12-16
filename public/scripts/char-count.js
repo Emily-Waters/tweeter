ac0444f2df46cc0d9c479e1d6801eb208029af64
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-16 23:59:17 +0000

    Cleaning

--- public/scripts/char-count.js
+++ public/scripts/char-count.js
@@ -1,5 +1,5 @@
 // Update counter val with length of string in the input box, change text color to red if over 140 chars, change back to inherited color if under 140 chars.
-$tweetInput.on('input', function() {
+$tweetInput.on('input', () => {
   // const textChars = $(this).val();
   const charCount = tweetLength($tweetInput);
   $counter.text(140 - charCount);
