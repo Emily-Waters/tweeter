f05b015a367ca6cf3510e117a00f5c39c0e01748
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-16 23:26:52 +0000

    More cleanup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,11 @@
 // Update counter val with length of string in the input box, change text color to red if over 140 chars, change back to inherited color if under 140 chars.
-$('#tweet-input').on('input', function() {
-  const textChars = $(this).val();
-  const charCount = textChars.length;
-  const $counter = $(this).next().children('#counter');
+$tweetInput.on('input', function() {
+  // const textChars = $(this).val();
+  const charCount = tweetLength($tweetInput);
   $counter.text(140 - charCount);
   if (charCount > 140) {
-    $(counter).css('color','red');
+    $counter.css('color','red');
   } else {
-    $(counter).css('color','inherit');
+    $counter.css('color','inherit');
   }
 });
