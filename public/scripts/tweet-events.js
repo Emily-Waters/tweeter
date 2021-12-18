95b8b9b407f2c1cb7164c0f3fc8406ec0977d75a
Emily-Waters <emilyabigailwaters@gmail.com>
2021-12-18 01:23:03 +0000

    cleanup

--- public/scripts/tweet-events.js
+++ public/scripts/tweet-events.js
@@ -1,10 +1,9 @@
-// Prevents default POST behaviour, validates the tweet contents and makes an ajax request, then fades out the tweets, empties and updates them, clears the input field, resets the counter, then fades back in. Tweets are checked for errors everytime because no error present will reset the error message.
+// Prevents default POST behaviour, validates the tweet contents and makes an ajax request, then fades out the tweets, prepends the newest tweet, clears the input field, resets the counter, then fades back in. Tweets are checked for errors everytime because no error present will reset the error message.
 $tweetForm.submit((event) => {
   event.preventDefault();
   const length = tweetLength($tweetInput);
   const tweetData = $tweetInput.serialize();
   const targetURL = event.currentTarget.action;
-  buttonBounce();
   if (tweetValidate(length)) {
     $.post(targetURL, tweetData)
       .done(() => {
@@ -21,6 +20,7 @@ $tweetForm.submit((event) => {
           });
       });
   }
+  buttonBounce();
   tweetError(length);
 });
 // Detects the 'enter' key being pressed in the tweet input form and submits the form instead of starting a new line
